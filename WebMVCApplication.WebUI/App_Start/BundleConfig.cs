ce7468c6f6ef6b171e1b7a5616c14c8f18ad4255
Wanchaloem Laoket <wanchaloem.laokeut@googlemail.com>
2019-05-22 23:34:20 +0700

    Update Template Add Dashboard page

--- WebMVCApplication.WebUI/App_Start/BundleConfig.cs
+++ WebMVCApplication.WebUI/App_Start/BundleConfig.cs
@@ -8,12 +8,52 @@ namespace WebMVCApplication.WebUI
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-            //          "~/Scripts/bootstrap.js"));
+            #region Assets
 
-            //bundles.Add(new StyleBundle("~/Content/css").Include(
-            //          "~/Content/bootstrap.css",
-            //          "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/styles/assets").Include(
+               "~/Assets/css/style.css"
+           ));
+
+            bundles.Add(new StyleBundle("~/scripts/assets").Include(
+                "~/Assets/js/main.js"
+            ));
+
+            #endregion
+
+            #region Main
+
+            bundles.Add(new StyleBundle("~/styles/main").Include(
+               "~/Vendors/bootstrap/dist/css/bootstrap.min.css",
+               "~/Vendors/font-awesome/css/font-awesome.min.css",
+               "~/Vendors/themify-icons/css/themify-icons.css",
+               "~/Vendors/flag-icon-css/css/flag-icon.min.css",
+               "~/Vendors/selectFX/css/cs-skin-elastic.css"
+           ));
+
+            bundles.Add(new ScriptBundle("~/scripts/main").Include(
+                "~/Vendors/jquery/dist/jquery.min.js",
+                "~/Vendors/popper.js/dist/umd/popper.min.js",
+                "~/Vendors/bootstrap/dist/js/bootstrap.min.js"
+            ));
+
+            #endregion
+
+            #region Dashboard 
+
+            bundles.Add(new ScriptBundle("~/styles/dashboard").Include(
+                "~/Vendors/jqvmap/dist/jqvmap.min.css"
+            ));
+
+            bundles.Add(new ScriptBundle("~/scripts/dashboard").Include(
+                "~/Vendors/chart.js/dist/Chart.bundle.min.js",
+                "~/Assets/js/dashboard.js",
+                "~/Assets/js/widgets.js",
+                "~/Vendors/jqvmap/dist/jquery.vmap.min.js",
+                "~/Vendors/jqvmap/examples/js/jquery.vmap.sampledata.js",
+                "~/Vendors/jqvmap/dist/maps/jquery.vmap.world.js"
+            ));
+
+            #endregion
         }
     }
 }
