a392dd5fc691bd87687752a2aa746acea1f9dca1
Wanchaloem Laoket <wanchaloem.laokeut@googlemail.com>
2019-05-22 22:25:42 +0700

    Add Sufee Admin Template

---
+++ WebMVCApplication.WebUI/Infrastructures/NinjectDependencyResolver.cs
@@ -0,0 +1,34 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebMVCApplication.WebUI.Infrastructures
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private readonly IKernel kernel;
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+
+        }
+    }
+}
